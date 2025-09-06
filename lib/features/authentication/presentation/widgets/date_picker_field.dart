144c3b871c47353949f7afd5ad1dc7dbf4a2b051
Z-Ash0 <zfire3005@gmail.com>
2025-09-06 23:33:09 +0300

    Finishing complete user data page, creating date picker, custom radio button, and phone number field widgets

---
+++ lib/features/authentication/presentation/widgets/date_picker_field.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:sejily/core/utils/app_colors.dart';
+import 'package:sejily/core/utils/app_text_styles.dart';
+
+class DatePickerField extends StatelessWidget {
+  final TextEditingController controller;
+  final VoidCallback onTap;
+
+  const DatePickerField({
+    super.key,
+    required this.controller,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          border: Border.all(color: AppColors.lightGray),
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+          child: Text(
+            controller.text.isEmpty ? '20/11/2004' : controller.text,
+            style: controller.text.isEmpty
+                ? AppTextStyles.regular16.copyWith(color: AppColors.gray)
+                : AppTextStyles.medium16.copyWith(color: AppColors.jetBlack),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class DatePickerHelper {
+  static Future<void> selectDate(
+    BuildContext context,
+    TextEditingController controller,
+    Function(String) onDateSelected,
+  ) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime(2004, 11, 20),
+      firstDate: DateTime(1970),
+      lastDate: DateTime.now(),
+      builder: (context, child) {
+        return Theme(
+          data: Theme.of(context).copyWith(
+            colorScheme: ColorScheme.light(
+              primary: AppColors.darkBlue,
+              onPrimary: AppColors.white,
+              surface: AppColors.white,
+              onSurface: AppColors.jetBlack,
+            ),
+          ),
+          child: child!,
+        );
+      },
+    );
+
+    if (picked != null) {
+      final formattedDate =
+          '${picked.day.toString().padLeft(2, '0')}/${picked.month.toString().padLeft(2, '0')}/${picked.year}';
+      controller.text = formattedDate;
+      onDateSelected(formattedDate);
+    }
+  }
+}
---
+++ lib/features/authentication/presentation/widgets/phone_number_field.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:country_code_picker/country_code_picker.dart';
+import 'package:sejily/core/utils/app_colors.dart';
+import 'package:sejily/core/utils/app_text_styles.dart';
+import 'package:sejily/core/utils/app_validators.dart';
+
+class PhoneNumberField extends StatefulWidget {
+  final TextEditingController controller;
+  final Function(String) onCountryCodeChanged;
+  final String initialCountryCode;
+
+  const PhoneNumberField({
+    super.key,
+    required this.controller,
+    required this.onCountryCodeChanged,
+    this.initialCountryCode = '+20',
+  });
+
+  @override
+  State<PhoneNumberField> createState() => _PhoneNumberFieldState();
+}
+
+class _PhoneNumberFieldState extends State<PhoneNumberField> {
+  late String _selectedCountryCode;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedCountryCode = widget.initialCountryCode;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: AppColors.lightGray),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Row(
+        children: [
+          CountryCodePicker(
+            onChanged: (country) {
+              setState(() {
+                _selectedCountryCode = country.dialCode!;
+              });
+              widget.onCountryCodeChanged(_selectedCountryCode);
+            },
+            initialSelection: 'EG',
+            favorite: const ['+20', 'EG'],
+            showCountryOnly: false,
+            showOnlyCountryWhenClosed: false,
+            alignLeft: false,
+            textStyle: AppTextStyles.medium16.copyWith(
+              color: AppColors.jetBlack,
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            flagDecoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+            ),
+          ),
+          Container(width: 1, height: 40, color: AppColors.lightGray),
+          Expanded(
+            child: TextFormField(
+              controller: widget.controller,
+              keyboardType: TextInputType.number,
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              style: AppTextStyles.medium16.copyWith(color: AppColors.jetBlack),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+                hintText: 'أدخل رقم هاتفك',
+                hintStyle: AppTextStyles.regular16.copyWith(
+                  color: AppColors.gray,
+                ),
+              ),
+              validator: AppValidators.phoneNumberValidator,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
