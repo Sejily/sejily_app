5f36fa0aedfee487567a5bbaf8577beaf50730e3
Z-Ash0 <zfire3005@gmail.com>
2025-09-12 03:28:35 +0300

    Updating api endpoints, creating ai api service

--- lib/core/constants/api_endpoints.dart
+++ lib/core/constants/api_endpoints.dart
@@ -1,4 +1,5 @@
 class ApiEndpoints {
+  //* Authentication
   static const String register = "/auth/register";
   static const String login = "/auth/login";
   static const String forgotPassword = "/auth/forgot-password";
@@ -10,4 +11,19 @@ class ApiEndpoints {
   static const String completePatientRegistration =
       "/auth/completeRegisterPatient";
   static const String refreshToken = "/auth/refresh";
+
+  //* AI
+  static const String aiUpload = '/ai/ai/file/upload';
+  static const String aiSearch = '/ai/search';
+  static const String aiFhirSearch = '/ai/fhir/search';
+  static const String aiDocumentHead = '/ai/document/';
+  static const String aiDocumentTail = '/extracted-data';
+  static const String aiInsights = '/ai/insights';
+
+  //* Document
+  static const String documents = '/documents';
+  static const String documentsStats = '/documents/stats';
+  static const String processingStatus = '/processing-status';
+  static const String documentSearch = '/documents/search';
+  static const String documentBulkDelete = '/documents/bulk/delete';
 }
---
+++ lib/core/services/ai_api_service.dart
@@ -0,0 +1,71 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+import 'package:sejily/core/constants/api_endpoints.dart';
+import 'package:sejily/features/ai/data/models/ai_fhir_search_response.dart';
+import 'package:sejily/features/ai/data/models/ai_search_request.dart';
+import 'package:sejily/features/ai/data/models/ai_search_response.dart';
+import 'package:sejily/features/ai/data/models/ai_upload_response.dart';
+import 'package:sejily/features/ai/data/models/bulk_deleted_document_request.dart';
+import 'package:sejily/features/ai/data/models/document_response.dart';
+import 'package:sejily/features/ai/data/models/document_search_response.dart';
+import 'package:sejily/features/ai/data/models/documents_response.dart';
+import 'package:sejily/features/ai/data/models/documents_stats_response.dart';
+import 'package:sejily/features/ai/data/models/processing_status_response.dart';
+
+part 'ai_api_service.g.dart';
+
+@RestApi()
+abstract class AiApiService {
+  factory AiApiService(Dio dio) = _AiApiService;
+
+  @POST(ApiEndpoints.aiUpload)
+  @MultiPart()
+  Future<AIUploadResponse> uploadFile(@Part() File file);
+
+  @POST(ApiEndpoints.aiSearch)
+  Future<AISearchResponse> searchFile({
+    @Body() required AISearchRequest aiSearchRequest,
+  });
+
+  @POST(ApiEndpoints.aiFhirSearch)
+  Future<AIFhirSearchResponse> fhirSearchFile({@Body() required String fileId});
+
+  @GET(
+    '${ApiEndpoints.aiDocumentHead}{documentId}${ApiEndpoints.aiDocumentTail}',
+  )
+  Future<void> getAIDocument({@Path() required String documentId});
+
+  @GET(ApiEndpoints.aiInsights)
+  Future<void> getInsights({@Query('limit') required int limit});
+
+  @GET(ApiEndpoints.documents)
+  Future<DocumentsResponse> getAllDocuments();
+
+  @GET(ApiEndpoints.documentsStats)
+  Future<DocumentsStatsResponse> getDocumentsStats();
+
+  @GET(ApiEndpoints.documentSearch)
+  Future<List<DocumentSearchResponse>> searchDocuments({
+    @Query('q') required String q,
+    @Query('limit') int? limit,
+  });
+
+  @GET('${ApiEndpoints.documents}/{id}')
+  Future<DocumentResponse> getDocument({@Path() required String id});
+
+  @DELETE('${ApiEndpoints.documents}/{id}')
+  Future<void> deleteDocument({@Path() required String id});
+
+  @GET('${ApiEndpoints.documents}/{id}/${ApiEndpoints.processingStatus}')
+  Future<ProcessingStatusResponse> getProcessingStatus({
+    @Path() required String id,
+  });
+
+  @DELETE(ApiEndpoints.documentBulkDelete)
+  Future<void> deleteDocuments({
+    @Body() required BulkDeletedDocumentRequest bulkDeletedDocumentRequest,
+  });
+}
